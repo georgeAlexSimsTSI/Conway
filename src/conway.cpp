8136d188f0aa8264c1c154210e37c28149a62782
George <gsims@softwareinstitute.com>
2022-05-20 12:00:39 +0100

    implemented getSurroundingCells

--- src/conway.cpp
+++ src/conway.cpp
@@ -1,18 +1,27 @@
 #include "../include/conway.h"
 
+#include <algorithm>
+
 using Coord = std::pair<unsigned int, unsigned int>; // doing this so that I get some practice
 using Grid = std::array<std::array<bool, yDimension>, xDimension>;
 
 // determine if the provided position is inside the grid
 bool Conway::isValidPosition(const Coord &pos)
 {
-    return (pos.first < 0 || pos.first >= xDimension && pos.second < 0 || pos.second >= yDimension);
+    auto [x, y] = pos;
+    return (x < 0 || x >= xDimension && y < 0 || y >= yDimension);
 }
 
 // return a vector containing coordinates of the surrounding cells that are inside the valid bounds
 std::vector<Coord> Conway::getSurroundingCells(const Coord &pos)
 {
-    return {};
+    auto [x, y] = pos;
+    std::vector<Coord> possiblePositions = {{x + 1, y}, {x - 1, y}, {x, y + 1}, {x, y - 1}, {x + 1, y - 1}, {x + 1, y + 1}, {x - 1, y + 1}, {x - 1, y - 1}};
+    std::vector<Coord> validPositions;
+
+    std::copy_if(possiblePositions.begin(), possiblePositions.end(), std::back_inserter(validPositions), isValidPosition);
+
+    return validPositions;
 }
 
 // get the sum value of the surrounding cells
